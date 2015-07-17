16be12e1a20d1cd1f83f5bb4ce7ee6aafd25ae10
Jari Aalto <jari.aalto@cante.net>
2015-07-17 11:31:41 +0300

    import original  CYGWIN-PATCHES 2015-07-17

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Copyright (C) 2007 Jari Aalto; Licenced under GPL v2 or later
+#
+# build.sh -- Custom build steps
+#
+#   Parameters send to this script
+#
+#       build.sh <PACKAGE> <VERSION> <RELEASE>
+
+env CC= make
+
+# End of file
